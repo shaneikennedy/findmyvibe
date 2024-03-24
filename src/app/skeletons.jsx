4cf84ea720f4455940f6961ce5b27158fe674954
Shane Kennedy <shane.kennedy19@gmail.com>
2024-03-24 14:41:11 +0100

    Use classname in skeleton loaders

--- src/app/skeletons.jsx
+++ src/app/skeletons.jsx
@@ -1,15 +1,15 @@
 function SongSkeleton() {
   return (
-    <div class="my-2 w-full rounded-md border border-gray-500 p-4 shadow">
-      <div class="flex animate-pulse space-x-4">
-        <div class="flex-1 space-y-6 py-1">
-          <div class="space-y-3">
-            <div class="grid grid-cols-3 gap-4">
-              <div class="col-span-1 h-2 rounded bg-gray-500"></div>
+    <div className="my-2 w-full rounded-md border border-gray-500 p-4 shadow">
+      <div className="flex animate-pulse space-x-4">
+        <div className="flex-1 space-y-6 py-1">
+          <div className="space-y-3">
+            <div className="grid grid-cols-3 gap-4">
+              <div className="col-span-1 h-2 rounded bg-gray-500"></div>
             </div>
-            <div class="grid grid-cols-9 gap-4">
-              <div class="col-span-8 h-2 rounded bg-gray-500"></div>
-              <div class="col-span-1 h-2 w-2 rounded-full bg-gray-500"></div>
+            <div className="grid grid-cols-9 gap-4">
+              <div className="col-span-8 h-2 rounded bg-gray-500"></div>
+              <div className="col-span-1 h-2 w-2 rounded-full bg-gray-500"></div>
             </div>
           </div>
         </div>
